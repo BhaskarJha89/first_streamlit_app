8128a349a35b5a1d848a15016d5d6c4eac744dd2
codeninja <53257283+BhaskarJha89@users.noreply.github.com>
2022-07-19 22:14:39 +0530

    Update streamlit_app.py
    Added fruits picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
-st.title('My Parents New Healthy Dinner')
-st.header('Breakfast Menu')
+st.title('Ankita Sajal Jha New Healthy Dinner')
+st.header('Breakfast Favorites')
 st.text('🥣 Omega 3 and Blueberry Oatmeal')
 st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include 
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 st.dataframe(my_fruit_list)
+
