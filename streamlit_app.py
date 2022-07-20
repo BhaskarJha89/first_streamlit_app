5d174e7e73677e95aa6ae950b9c80c44e00b95c6
codeninja <53257283+BhaskarJha89@users.noreply.github.com>
2022-07-20 15:49:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import snowflake.connector
 st.title('Ankita Sajal New Healthy Breakfast')
 st.header('Breakfast Favorites')
 st.text('🥣 Omega 3 and Blueberry Oatmeal')
